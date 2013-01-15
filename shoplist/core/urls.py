da1a154e5b51da82cfffa9d4dfaa5142a3fbf3c0
Luis Roberto Pereira de Paula <luisrpp@gmail.com>
2013-01-15 18:38:35 -0200

    Update ShopList name with ajax call

--- shoplist/core/urls.py
+++ shoplist/core/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('shoplist.core.views',
     url(r'^shoplist$', 'shoplist', name='shoplist'),
+    url(r'^shoplist/update$', 'update_shoplist', name='update_shoplist'),
 )
+
--- shoplist/core/views.py
+++ shoplist/core/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.db.models import Count 
+from django.http import HttpResponse
+from django.db.models import Count
+from django.core import serializers
 from models import ShopList
 from forms import ShopListForm
 
@@ -16,10 +18,12 @@ def shoplist(request):
     else:
         return _new_shoplist(request, shoplists)
 
+
 def _new_shoplist(request, shoplists=None):
     form = ShopListForm()
     context = RequestContext(request, {'form': form, 'shoplists': shoplists})
     return render_to_response('core/shoplist.html', context)
+
 
 def _create_shoplist(request, shoplists=None):
     form = ShopListForm(request.POST)
@@ -33,3 +37,15 @@ def _create_shoplist(request, shoplists=None):
     #TODO: Send to another page to create list items
     context = RequestContext(request, {'form': form, 'shoplists': shoplists},)
     return render_to_response('core/shoplist.html', context)
+
+
+def update_shoplist(request):
+    if request.is_ajax():
+        sl = ShopList.objects.get(id=request.GET.get('id'))
+        sl.name = request.GET.get('name')
+        sl.save()
+
+        data = serializers.serialize('json', [sl,])
+
+        return HttpResponse(data, mimetype='application/json')
+
